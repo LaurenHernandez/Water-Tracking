b469c4ef0dcba7ca41d1eeaa0e0757d7823e1ac1
LaurenHernandez <42851107+LaurenHernandez@users.noreply.github.com>
2018-09-25 17:13:32 -0600

    Add files via upload

---
+++ Water Tracking/WindowsFormsApp1/WaterTracking.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class WaterTracking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumberConsumed = new System.Windows.Forms.TextBox();
+            this.txtWaterGoal = new System.Windows.Forms.TextBox();
+            this.txtOuncesRemaining = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Number of Ounces Consumed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Water &Goal (in gallons)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ounces Remaining";
+            // 
+            // txtNumberConsumed
+            // 
+            this.txtNumberConsumed.Location = new System.Drawing.Point(245, 38);
+            this.txtNumberConsumed.Name = "txtNumberConsumed";
+            this.txtNumberConsumed.Size = new System.Drawing.Size(100, 20);
+            this.txtNumberConsumed.TabIndex = 1;
+            // 
+            // txtWaterGoal
+            // 
+            this.txtWaterGoal.Location = new System.Drawing.Point(245, 79);
+            this.txtWaterGoal.Name = "txtWaterGoal";
+            this.txtWaterGoal.Size = new System.Drawing.Size(100, 20);
+            this.txtWaterGoal.TabIndex = 2;
+            // 
+            // txtOuncesRemaining
+            // 
+            this.txtOuncesRemaining.Location = new System.Drawing.Point(245, 127);
+            this.txtOuncesRemaining.Name = "txtOuncesRemaining";
+            this.txtOuncesRemaining.ReadOnly = true;
+            this.txtOuncesRemaining.Size = new System.Drawing.Size(100, 20);
+            this.txtOuncesRemaining.TabIndex = 5;
+            this.txtOuncesRemaining.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(108, 203);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(245, 203);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // WaterTracking
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(414, 264);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtOuncesRemaining);
+            this.Controls.Add(this.txtWaterGoal);
+            this.Controls.Add(this.txtNumberConsumed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "WaterTracking";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Water Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumberConsumed;
+        private System.Windows.Forms.TextBox txtWaterGoal;
+        private System.Windows.Forms.TextBox txtOuncesRemaining;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
